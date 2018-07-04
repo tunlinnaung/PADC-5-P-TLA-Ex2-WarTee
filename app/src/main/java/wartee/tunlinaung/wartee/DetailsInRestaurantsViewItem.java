e53b01c9d679c0ba310a079a300fb8c3fc4533e5
tunlinaung <tunlinaung.java@gmail.com>
2018-07-04 23:13:00 +0630

    setup list screen

---
+++ app/src/main/java/wartee/tunlinaung/wartee/DetailsInRestaurantsViewItem.java
@@ -0,0 +1,21 @@
+package wartee.tunlinaung.wartee;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class DetailsInRestaurantsViewItem extends FrameLayout {
+    public DetailsInRestaurantsViewItem(@NonNull Context context) {
+        super(context);
+    }
+
+    public DetailsInRestaurantsViewItem(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public DetailsInRestaurantsViewItem(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
